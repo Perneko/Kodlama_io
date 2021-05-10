a93fd588b355c0809261f7399720f7b7d4b17bc9
Taha Emre UĞUR <44301010+Perneko@users.noreply.github.com>
2021-05-10 16:14:55 +0300

    hafta6_ödev3

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Job.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="jobs")
+@Data
+public class Job {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="job_id")
+	private int id;
+	
+	@Column(name="job_name")
+	private String jobName;
+	
+	public Job() {}
+
+	public Job(int id, String jobName) {
+		super();
+		this.id = id;
+		this.jobName = jobName;
+	}
+	
+	
+}
